6adb23167f3d59d7abe61630d655497b16acd536
ClementJehanno <clement.jehanno@etu.univ-nantes.fr>
2018-10-16 16:31:53 +0200

    update insertion. A exec dans sage-engine

--- scriptsPython/insertionHDT.py
+++ scriptsPython/insertionHDT.py
@@ -1,4 +1,5 @@
 from cassandra.cluster import Cluster
+from hdt import HDTDocument
 
 cluster = Cluster()
 session = cluster.connect()
@@ -6,7 +7,7 @@ session = cluster.connect()
 # Creating keyspace
 session.execute(
     """
-    CREATE KEYSPACE IF NOT EXISTS testhdt WITH REPLICATION = {
+    CREATE KEYSPACE IF NOT EXISTS testdata WITH REPLICATION = {
         'class' : 'SimpleStrategy',
         'replication_factor' : 1
     }
@@ -14,11 +15,11 @@ session.execute(
 )
 
 # on switch sur le bon KEYSPACE
-session.set_keyspace('testhdt')
+session.set_keyspace('testdata')
 
 session.execute(
     """
-    CREATE TABLE IF NOT EXISTS testhdt.records (
+    CREATE TABLE IF NOT EXISTS records (
     sujet text,
 	predicat text,
 	objet text,
@@ -27,12 +28,11 @@ session.execute(
     """
 )
 
-
-# session.execute(
-# 	"""
-# 	CREATE INDEX IF NOT EXISTS indexSujet ON testhdt.records ( KEYS (sujet) );
-# 	"""
-# )
+#session.execute(
+#	"""
+#	CREATE INDEX IF NOT EXISTS indexSujet ON testhdt.records ( KEYS (sujet) );
+#	"""
+#)
 #session.execute(
 #	"""
 #	CREATE INDEX IF NOT EXISTS indexPredicat ON testhdt.records ( KEYS (predicat) );
@@ -44,21 +44,20 @@ session.execute(
 #	"""
 #)
 
-
-from hdt import HDTDocument
-
- # Load an HDT file. Missing indexes are generated automatically
-document = HDTDocument("swdf.hdt")
+# Load an HDT file. Missing indexes are generated automatically
+document = HDTDocument("tests/data/test.hdt")
 
 # Fetch all triples that matches { ?s ?p ?o }
 # Use empty strings ("") to indicates variables
 (triples, cardinality) = document.search_triples("", "", "")
 
+print(cardinality)
+exit()
 # print("cardinality of { ?s ?p ?o }: %i" % 10)
-for triple in zip( range(20), triples):
-    test = "INSERT INTO testhdt.records (sujet, predicat, objet) VALUES (\'" + triple[1][0] + "\', \'" + triple[1][1] + "\', \'" + triple[1][2] + "\');"
+for triple in zip( range(cardinality), triples):
+    test = "INSERT INTO records (sujet, predicat, objet) VALUES (\'" + triple[1][0] + "\', \'" + triple[1][1] + "\', \'" + triple[1][2] + "\');"
     print(test)
-    session.execute(test)   # A opti parce que imo c'est naze
+    session.execute(test)
 
-# En fait ici j'ai un truc INDEX qui se rajoute, donc je tape ssur triple[1] pour aller chercher ma data
+#En fait ici j'ai un truc INDEX qui se rajoute, donc je tape ssur triple[1] pour aller chercher ma data
 # triple[0] = mon index
