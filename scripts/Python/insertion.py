38dcb295047408767f6cd0d4e6ee5a991cb5f955
ClementJehanno <clement.jehanno@etu.univ-nantes.fr>
2018-10-17 11:50:25 +0200

    Insertion + queries sur TOKEN

--- scripts/Python/insertion.py
+++ scripts/Python/insertion.py
@@ -1,13 +1,18 @@
 from cassandra.cluster import Cluster
 from hdt import HDTDocument
 
+
+# Dans ce script on fait une insertion sur une primary key complexe afin de pouvoir faire des requetes avec nos tokens derrière
+
+
+
 cluster = Cluster()
 session = cluster.connect()
 
 # Creating keyspace
 session.execute(
     """
-    CREATE KEYSPACE IF NOT EXISTS testdata WITH REPLICATION = {
+    CREATE KEYSPACE IF NOT EXISTS jekasstout WITH REPLICATION = {
         'class' : 'SimpleStrategy',
         'replication_factor' : 1
     }
@@ -15,7 +20,7 @@ session.execute(
 )
 
 # on switch sur le bon KEYSPACE
-session.set_keyspace('testdata')
+session.set_keyspace('jekasstout')
 
 session.execute(
     """
@@ -23,7 +28,8 @@ session.execute(
     sujet text,
 	predicat text,
 	objet text,
-	PRIMARY KEY (sujet, predicat, objet)
+    id int,
+	PRIMARY KEY ((sujet, predicat, objet), id)
     );
     """
 )
@@ -52,12 +58,16 @@ document = HDTDocument("tests/data/test.hdt")
 (triples, cardinality) = document.search_triples("", "", "")
 
 print(cardinality)
-exit()
+
+i = 0
+
 # print("cardinality of { ?s ?p ?o }: %i" % 10)
 for triple in zip( range(cardinality), triples):
-    test = "INSERT INTO records (sujet, predicat, objet) VALUES (\'" + triple[1][0] + "\', \'" + triple[1][1] + "\', \'" + triple[1][2] + "\');"
-    print(test)
+    test = "INSERT INTO records (sujet, predicat, objet, id) VALUES (\'" + triple[1][0] + "\', \'" + triple[1][1] + "\', \'" + triple[1][2] + "\' , " + str(i) + " );"
+    i = i+1
+#    print(test)
     session.execute(test)
 
+print("done")
 #En fait ici j'ai un truc INDEX qui se rajoute, donc je tape ssur triple[1] pour aller chercher ma data
 # triple[0] = mon index
