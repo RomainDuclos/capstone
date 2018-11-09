9e54d906576c7f772a0ac0a361809e327e7ce52c
Etilea <clementdudeux@free.fr>
2018-11-09 10:18:24 +0100

    benchmark cluster

---
+++ scripts/Python/benchmarkRealCluster.py
@@ -0,0 +1,55 @@
+from cassandra.cluster import Cluster
+import time
+import statistics
+
+GlobalStart = time.time()
+
+cluster = Cluster(['172.16.134.144', '172.16.134.142', '172.16.134.143'])
+session = cluster.connect()
+# on switch sur le bon KEYSPACE
+session.set_keyspace('testcluster')
+
+#variables
+nbTotalLignesTheorique = 1000000
+nbRange = 10000
+nbIter = nbTotalLignesTheorique/nbRange
+listTimers = []
+
+
+for i in range(nbIter):
+    if i==0 :
+        benchmark = "SELECT sujet, predicat, objet, token(sujet, predicat, objet) from records limit " + str(nbRange) + ";"
+    else :
+        benchmark = "SELECT sujet, predicat, objet, token(sujet, predicat, objet) from records where token(sujet, predicat, objet) > " + str(token) + " limit " + str(nbRange) + ";"
+    start = time.time()
+    maRange = session.execute(benchmark)    #on recup tout
+    end = time.time()
+    #liste des temps pour stats
+    listTimers.append(end-start)
+    #On va chercher le dernier token et on le charge
+    # le ifelse est pour eviter que ca plante
+    if maRange:
+        token = maRange[-1][-1]
+    else:
+        break
+    # print(token)
+
+
+# stats
+i=0
+for timer in listTimers:
+    i = i+1
+    monTimer = str(timer)
+    while(len(monTimer)<15):
+        monTimer += "0"
+    print(monTimer + " seconds")
+
+GlobalEnd = time.time()
+
+print("total de " + str(i) + " tokens lus sur des ranges de " + str(nbRange) + " lignes")
+print("Temps global d'execution du programme : " + str(GlobalEnd-GlobalStart) + " seconds")
+#moyenne=statistics.median(listTimers)
+#max = max(listTimers)
+#min = min(listTimers)
+#print(moyenne)
+#print(max-min)
