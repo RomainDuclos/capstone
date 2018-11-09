5e944a0facd40b76f60d54a03cbbb5e4fe5b2415
ClementJehanno <clement.jehanno@etu.univ-nantes.fr>
2018-11-09 18:57:58 +0100

    insertion + rapide

--- scripts/Python/insertionClusterFromNT.py
+++ scripts/Python/insertionClusterFromNT.py
@@ -1,10 +1,15 @@
 from cassandra.cluster import Cluster
 from hdt import HDTDocument
-from cassandra.policies import DCAwareRoundRobinPolicy  
+from cassandra.policies import DCAwareRoundRobinPolicy
+from cassandra.query import BatchStatement
+from cassandra.query import SimpleStatement
+import time
 
-cluster = Cluster(
-    ['172.16.134.144', '172.16.134.142', '172.16.134.143'],
-    load_balancing_policy=DCAwareRoundRobinPolicy(local_dc='dc1'))
+# cluster = Cluster(
+#     ['172.16.134.144', '172.16.134.142', '172.16.134.143'],
+#     load_balancing_policy=DCAwareRoundRobinPolicy(local_dc='dc1'))
+cluster = Cluster()
+
 
 session = cluster.connect()
 
@@ -14,7 +19,7 @@ session = cluster.connect()
 # Creating keyspace
 session.execute(
     """
-    CREATE KEYSPACE IF NOT EXISTS test4 WITH REPLICATION = {
+    CREATE KEYSPACE IF NOT EXISTS test WITH REPLICATION = {
         'class' : 'SimpleStrategy',
         'replication_factor' : 1
     }
@@ -22,7 +27,7 @@ session.execute(
 )
 
 # on switch sur le bon KEYSPACE
-session.set_keyspace('test4')
+session.set_keyspace('test')
 
 session.execute(
     """
@@ -54,15 +59,26 @@ session.execute(
 i=0
 data = open("testdata.nt")
 
+insert = "BEGIN BATCH "
+
+GlobalStart = time.time()
+batch = BatchStatement()
+
 for line in data:
     i = i+1
     triple = line.split(' ')
     triple[2] = triple[2].rstrip()
     test = "INSERT INTO records (sujet, predicat, objet) VALUES ($$" + triple[0].replace("$", "\$") + "$$, $$" + triple[1].replace("$", "\$") + \
     "$$, $$" + triple[2].replace("$", "\$") + "$$ );"
-    session.execute(test)
-    print(i)
-    print(test)
+    batch.add(SimpleStatement(test))
+    if(i%10000==0):
+        session.execute(batch)
+        batch = BatchStatement()
+        print("row inserted : " + str(i))
+
+
+GlobalEnd = time.time()
+print("temps total " + str(GlobalEnd-GlobalStart) + " " + str(i) +" lignes inserees")
 
 # i=0
 # # print("cardinality of { ?s ?p ?o }: %i" % 10)
