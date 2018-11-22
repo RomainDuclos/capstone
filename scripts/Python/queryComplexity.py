95140aa3e5c296fe609f471087135b2d3d1cc4a8
Pierre CAILLAUD <pierre_caillaud@yahoo.fr>
2018-11-22 14:19:34 +0100

    time complexity on SPO, POS and OSP

--- scripts/Python/queryComplexity.py
+++ scripts/Python/queryComplexity.py
@@ -4,43 +4,44 @@ import time
 cluster = Cluster(['172.16.134.144', '172.16.134.142', '172.16.134.143'])
 session = cluster.connect()
 
-session.set_keyspace('ourdataset')
+session.set_keyspace('pko')
 
 def queryComplexity(query, name):
     start = time.time()*1000
     resp = session.execute(query)
     end = time.time()*1000
-    print("\n"+name+"\n")
-    print("query: \t")
-    print("time: \t"+str(end-start)+" ms")
-    # nb = 0
-    # for r in resp:
-    #     nb += 1
-    # print("nb: "+str(nb)+"\n")
+    nb = 0
+    for r in resp:
+        nb += 1
+    print(name+","+str(end-start)+","+str(nb))
 
-
+print("#id, time, nbResult")
 
 # SPO
-query = "SELECT * FROM records WHERE sujet='a' and predicat='b' and objet='c' ALLOW FILTERING"
+query = "SELECT * FROM records WHERE sujet='a' and predicat='b' and objet='c1' ALLOW FILTERING"
 queryComplexity( query, "SPO")
 # S..
-query = "SELECT * FROM records WHERE sujet='a' ALLOW FILTERING;"
-queryComplexity( query, "S..")
-# SP.
-query = "SELECT * FROM records WHERE sujet='a' and predicat='b' ALLOW FILTERING;"
-queryComplexity( query, "SP.")
+# query = "SELECT * FROM records WHERE sujet='a' ALLOW FILTERING;"
+# queryComplexity( query, "S..")
+# # SP.
+# query = "SELECT * FROM records WHERE sujet='a' and predicat='b' ALLOW FILTERING;"
+# queryComplexity( query, "SP.")
 # S.O
 query = "SELECT * FROM records WHERE sujet='a' and objet='c' ALLOW FILTERING;"
 queryComplexity( query, "S.O")
-# .P.
-query = "SELECT * FROM records WHERE predicat='b' ALLOW FILTERING;"
-queryComplexity( query, ".P.")
-# .PO
-query = "SELECT * FROM records WHERE predicat='b' and objet='c' ALLOW FILTERING;"
-queryComplexity( query, ".PO")
+# #.P.
+# query = "SELECT * FROM records WHERE predicat='b' ALLOW FILTERING;"
+# queryComplexity( query, ".P.")
+# # #.PO
+# query = "SELECT * FROM records WHERE predicat='b' and objet='c' ALLOW FILTERING;"
+# queryComplexity( query, ".PO")
 # ..O
 query = "SELECT * FROM records WHERE objet='c' ALLOW FILTERING;"
 queryComplexity( query, "..O")
-# ...
-query = "SELECT * FROM records;"
-queryComplexity( query, "...")
+# # ...
+# query = "SELECT * FROM records;"
+# queryComplexity( query, "...")
+
+
+# query = "SELECT count(*) FROM records ALLOW FILTERING;"
+# queryComplexity( query, "COUNT")
