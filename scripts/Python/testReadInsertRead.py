d291c2ad7b15f8f2e3986051f7e85c01101d09ab
ClementJehanno <clement.jehanno@etu.univ-nantes.fr>
2018-12-19 22:29:45 +0100

    test lecture insert lecture

---
+++ scripts/Python/testReadInsertRead.py
@@ -0,0 +1,56 @@
+from cassandra.cluster import Cluster
+import time
+import statistics
+from cassandra.query import SimpleStatement
+
+
+# cluster = Cluster(['172.16.134.144', '172.16.134.142', '172.16.134.143'])
+cluster = Cluster()
+session = cluster.connect()
+
+session.set_keyspace('pktest')
+
+#Le paging state permet de recuperer a partir de la prochaine page, mais ca suppose qu'on a eu le temps de lire en entier notre page sinon c'est mmort
+# Estt-ce que l'acces au paging state est temps constant, si oui combien on met de temps dans le paging state a revenir ou on etait ?
+
+#Acces temps constant ? =>
+
+#On fait un tour, on stop, et on recommence
+query = "SELECT * FROM records"
+tailleFetch = 7
+# statement = SimpleStatement(query, fetch_size=2000)
+statement = SimpleStatement(query, fetch_size=tailleFetch)
+res = session.execute(statement)
+
+cpt=0
+courant = ""
+for l in res:
+    print(l)
+    courant = res.paging_state
+    # print(courant)
+    if(cpt>=6):
+        break;
+    cpt = cpt+1
+
+print("---------------------------------------------------------")
+
+session.execute("INSERT INTO records (sujet, predicat, objet) VALUES ('a', 'b6', 'c');")
+session.execute("INSERT INTO records (sujet, predicat, objet) VALUES ('a', 'b7', 'c');")
+session.execute("INSERT INTO records (sujet, predicat, objet) VALUES ('a', 'b8', 'c');")
+session.execute("INSERT INTO records (sujet, predicat, objet) VALUES ('a', 'b9', 'c');")
+session.execute("INSERT INTO records (sujet, predicat, objet) VALUES ('a', 'b10', 'c');")
+# session.execute("DELETE FROM records WHERE sujet='a' and predicat='b3'")
+# session.execute("DELETE FROM records WHERE sujet='a' and predicat='b4'")
+# session.execute("DELETE FROM records WHERE sujet='a' and predicat='b5'")
+
+
+cpt=0
+statement = SimpleStatement(query, fetch_size=20)
+res2 = session.execute(statement, paging_state=courant)
+for l2 in res2:
+    print(l2)
+    courant = res2.paging_state
+    # print(courant)
+    if(cpt>=15):
+        break;
+    cpt = cpt+1
